e7a34fe2d9ea7ce91b55eb5e66c1309986c347c1
lami <lamibabu03@gmail.com>
2025-06-13 11:14:33 +0530

    start

--- components/form/Loginform.jsx
+++ components/form/Loginform.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
-function Loginform() {
+function Loginform(props) {
+  console.log("Loginform props", props);
   return (
-    <div>Loginform</div>
+    <>
+    <h1>Logindata</h1>
+    <p>Username: {props.formname}</p>
+    <p>Password:{props.formpassword}</p>
+    </>
   )
 }
 
--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -1,9 +1,39 @@
+import Loginform from '@/components/form/Loginform'
 import React from 'react'
-import Loginform form '../../'
+import {useState} from "react"
 
 function index() {
+    const [name,  setName]=useState("");
+  console.log("Name",name);
+  const [password, setPassword]=useState("");
+  console.log("Password",password);
+  const[submit, setSubmit]=useState("");
+  console.log("submit",submit);
+  function handleName(event){
+    setName(event.target.value);
+  }
+  function handlePass(event){
+    setPassword(event.target.value);
+  }
+  function handleSubmit(){
+    if (password.length < 8 ){
+      alert("password must be at least 8 characters")
+      return;
+      console.log('success');
+    }
+  }
   return (
-    <Loginform/>
+    <>
+     <h1>Hi {state}</h1>
+     <h2> your email{state}</h2>
+     <input type='text' onChange={handleName}/>
+     <p>enter your username</p><input type='text' onChange={handleName}/>
+     <p>enter your password</p><input type='password' onChange={handlePass}/>
+     <br/>
+     <button onClick={handleSubmit}>submit</button>
+     <Loginform formname={name} formpassword={password}/>
+
+    </>
   )
 }
 
